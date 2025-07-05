6fc6a44f8b0bea88972feb877cc12799f72d2efa
AGRIM ASWAL <124095607+agrimaswal@users.noreply.github.com>
2025-07-05 15:50:28 +0530

    Update ValidParentheses.java

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -3,24 +3,25 @@ import java.util.Stack;
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
+
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
             } else {
                 if (stack.isEmpty()) {
                     return false;
                 }
-                
+
                 char top = stack.pop();
-                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+
+                if (ch == ')' && top != '(' || ch == '}' && top != '{' || ch == ']' && top != '[') {
                     return false;
                 }
+
             }
         }
         
         return stack.isEmpty();
     }
 }
-
-
